add49fb84b14e86ed94b001fffc054f5631b737c
zevwax <zevwax@zevwax.com>
2026-06-14 23:15:09 +0000

    0.13.1 implementend challenges; started working on game sequencer

---
+++ Assets/My/Scripts/GLegacy.cs
@@ -0,0 +1,23 @@
+/*using UnityEngine;
+using System.Linq;
+using System.Collections;
+namespace ZevWaxGames.CursorHero
+{
+    public class G : MonoBehaviour // reminder: implement a game sequencer w/ async/await
+    {
+        public static G Instance { get; private set; }
+        public Button bin;
+        public Button net;
+        private void Awake() => Instance = this;
+        private void Start()
+        {
+            //Screen.SetResolution(300, 900, FullScreenMode.Windowed);
+            //Screen.SetResolution(320, 180, FullScreenMode.FullScreenWindow);
+            //Screen.fullScreen = true;
+            //PlayerPrefs.DeleteAll();
+            
+            
+            
+        }
+    }
+}*/
